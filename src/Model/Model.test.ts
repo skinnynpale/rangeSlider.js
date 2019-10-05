9ca02abc2067982decdfddf99b0f6e6e66b8f496
skinnynpale <skinnynpale@gmail.com>
2019-10-05 20:28:23 +0700

    Написаны еще 5 тестов для Модели

--- src/Model/Model.test.ts
+++ src/Model/Model.test.ts
@@ -15,4 +15,75 @@ describe("Model", () => {
     expect(model.state.values).to.deep.equal([50]);
     expect(model.state.step).to.equal(2);
   });
+  it("Должен исправить min/max если заходят друг за друга", () => {
+    const model = new Model({
+      min: 90,
+      max: 80,
+      values: [60],
+    });
+
+    expect(model.state.min).to.equal(80);
+    expect(model.state.max).to.equal(90);
+
+    model.setState({ min: 10, max: -5 });
+
+    expect(model.state.min).to.equal(-5);
+    expect(model.state.max).to.equal(10);
+  });
+  it("Должен исправить шаг", () => {
+    const model = new Model({
+      step: 90,
+      max: 50,
+      values: [60],
+    });
+
+    expect(model.state.step).to.equal(50);
+
+    model.setState({ step: -1 });
+
+    expect(model.state.step).to.eq(1);
+  });
+  it("Должен откорректировать значение отталкиваясь от шага", () => {
+    const model = new Model({
+      min: 10,
+      max: 80,
+      values: [47],
+      step: 2,
+    });
+
+    expect(model.state.values).to.deep.eq([48]);
+  });
+  it("Должен вернуть откорректированное значение отталкиваясь если оно заходит за min/max", () => {
+    const model = new Model({
+      min: 10,
+      max: 79,
+      values: [89],
+      step: 2,
+    });
+
+    expect(model.state.values).to.deep.eq([78]);
+
+    model.setState({ values: [5], min: 11 });
+    expect(model.state.values).to.deep.eq([12]);
+
+    model.setState({
+      min: 10,
+      max: 80,
+      values: [0],
+      step: 3,
+    });
+
+    expect(model.state.values).to.deep.eq([12]);
+  });
+  it("Должен все сделать в правильном порядке", () => {
+    const model = new Model({
+      min: 50,
+      max: -200,
+      values: [-31],
+      step: 6,
+    });
+
+    expect(model.state.min).to.eq(-200);
+    expect(model.state.max).to.eq(50);
+  });
 });
--- src/Model/Model.ts
+++ src/Model/Model.ts
@@ -1,5 +1,5 @@
 interface IState {
-  [key: string]: number;
+  [key: string]: number | number[];
 }
 
 class Model {
@@ -11,6 +11,51 @@ class Model {
 
   public setState(state = {}): void {
     Object.assign(this.state, state);
+
+    this._correctMinMaxRange();
+    this._correctStep();
+    this.state.values = (this.state.values as number[]).map(value => this._correctValueInTheRange(value));
+    this.state.values = (this.state.values as number[]).map(value => this._correctValueByStep(value));
+
+    console.log(this.state);
+  }
+
+  private _correctMinMaxRange(): void {
+    if (this.state.min > this.state.max) {
+      const temp = this.state.min;
+      this.state.min = this.state.max;
+      this.state.max = temp;
+    }
+  }
+
+  private _correctStep(): void {
+    this.state.step < 1 ? (this.state.step = 1) : "";
+    this.state.step > this.state.max ? (this.state.step = this.state.max) : "";
+  }
+
+  private _correctValueInTheRange(value: number): number {
+    return this._isValueInTheRange(value);
+  }
+
+  private _isValueInTheRange(value: number): number {
+    if (value < this.state.min) {
+      return this.state.min as number;
+    } else if (value > this.state.max) {
+      return this.state.max as number;
+    } else {
+      return value;
+    }
+  }
+
+  private _correctValueByStep(value: number): number {
+    const step = this.state.step as number;
+    const newValue = Math.ceil(value / step) * step;
+
+    if (newValue > this.state.max) {
+      return Math.floor(value / step) * step;
+    } else {
+      return newValue;
+    }
   }
 }
 
